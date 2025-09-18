eceb2e173b0a086d4e2487fc1c30fb5094e6fb71
DeepeshSuryawanshi-CreArt <deepesh@creartsolutions.com>
2025-09-18 15:23:57 +0530

    delete the copilot seed and make all seeds working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
-            PermissionSeeder::class,
+            PermissionsSeeder::class,
             RoleSeeder::class,
             AdminUserSeeder::class
         ]);
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,66 +14,26 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        // Clear cache
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        // Create Admin role with all permissions
+        $adminRole = Role::create(['name' => 'Admin']);
+        $adminRole->givePermissionTo(Permission::all());
 
-        // Create sample roles
-        $roles = [
-            [
-                'name' => 'Super Admin',
-                'guard_name' => 'web',
-                'permissions' => 'all' // Will get all permissions
-            ],
-            [
-                'name' => 'Admin',
-                'guard_name' => 'web',
-                'permissions' => [
-                    'manage-users', 'create-users', 'edit-users', 'view-users',
-                    'manage-roles', 'view-roles',
-                    'view-permissions',
-                    'view-dashboard', 'view-analytics',
-                    'manage-settings'
-                ]
-            ],
-            [
-                'name' => 'Manager',
-                'guard_name' => 'web',
-                'permissions' => [
-                    'view-users', 'edit-users',
-                    'view-roles',
-                    'view-permissions',
-                    'view-dashboard', 'view-analytics'
-                ]
-            ],
-            [
-                'name' => 'User',
-                'guard_name' => 'web',
-                'permissions' => [
-                    'view-dashboard'
-                ]
-            ],
-        ];
+        // Create Manager role with limited permissions
+        $managerRole = Role::create(['name' => 'Manager']);
+        $managerRole->givePermissionTo([
+            'user_view',
+            'user_edit',
+            'role_view',
+            'permission_view',
+            'activity_view',
+        ]);
 
-        foreach ($roles as $roleData) {
-            $role = Role::create([
-                'name' => $roleData['name'],
-                'guard_name' => $roleData['guard_name']
-            ]);
-
-            // Assign permissions to role
-            if ($roleData['permissions'] === 'all') {
-                // Give all permissions to Super Admin
-                $role->givePermissionTo(Permission::all());
-            } elseif (is_array($roleData['permissions'])) {
-                // Give specific permissions to role
-                foreach ($roleData['permissions'] as $permissionName) {
-                    $permission = Permission::where('name', $permissionName)->first();
-                    if ($permission) {
-                        $role->givePermissionTo($permission);
-                    }
-                }
-            }
-        }
+        // Create User role with basic permissions
+        $userRole = Role::create(['name' => 'User']);
+        $userRole->givePermissionTo([
+            'user_view',
+            'activity_view',
+        ]);
 
         $this->command->info('Sample roles with permissions created successfully!');
     }
